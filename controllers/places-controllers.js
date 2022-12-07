d025ef119381dce1311db85ef43259a8ee85af04
Jason Whisnant <tasups@gmail.com>
2022-12-07 20:02:16 +0000

    deletePlace created and tested

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -71,7 +71,9 @@ const updatePlace = (req, res, next) => {
 }
 
 const deletePlace = (req, res, next) => {
-  
+  const placeId = req.params.id
+  DUMMY_PLACES.filter(p => p.id !== placeId)
+  res.status(200).json({ message: 'Deleted place.'})
 }
 
 exports.getPlaceById = getPlaceById
